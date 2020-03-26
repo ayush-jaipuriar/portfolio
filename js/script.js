4b99a0621af2ddf5b8de322f79e8ede25f9f6b72
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2020-03-26 13:31:43 +0530

    Added typedJS

--- js/script.js
+++ js/script.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
     play: 3000,
     pagination: false
   });
+
+  var typed = new Typed(".typed ", {
+    strings: [
+      "ANDROID APP DEVELOPER",
+      "FULL STACK WEB DEVELOPER",
+      "COMPUTER SCIENCE AND ENGINEERING 3RD YEAR STUDENT"
+    ],
+    typeSpeed: 25,
+    fadeOut: false,
+    fadeOutClass: "typed-fade-out",
+    fadeOutDelay: 500,
+
+    startDelay: 1000,
+    backSpeed: 25,
+    backDelay: 1000,
+    smartBackspace: true,
+    loop: true,
+    showCursor: false
+  });
 });
